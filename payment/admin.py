945345816031b5387554297bb8a89ab1b61f7dad
shukrullo1303 <bekbek791@gmail.com>
2026-01-02 01:46:11 +0500

    workin

--- payment/admin.py
+++
@@ -1,8 +0,0 @@
-# payment/admin.py
-from django.contrib import admin
-from .models import Payment
-
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('session', 'amount', 'paid_at')
-    search_fields = ('session__table__number',)
