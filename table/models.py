f6b924ba83ab5637865b13129de9c9e62c4c0149
shukrullo1303 <bekbek791@gmail.com>
2026-01-03 21:19:16 +0500

    Remove SQLite DB from repo

--- table/models.py
+++ table/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Table(models.Model):
     number = models.PositiveIntegerField(unique=True)
     table_type = models.CharField(max_length=50, default="Standard")
-    price_per_hour = models.DecimalField(max_digits=10, decimal_places=2)
+    price_per_hour = models.DecimalField(max_digits=10, decimal_places=0)
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
